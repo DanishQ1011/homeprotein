febba115b700ec2e73810c0c5f9f312242c0dc37
Shah Danish <shah.official1011@gmail.com>
2023-12-20 21:04:53 +0530

    HomeProtein Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from '@/assets/logo.png'
+
+export default function Navbar(){
+    return(
+        <nav className="py-4 px-9 mx-40">
+            <div className="flex justify-between items-center">
+                <div>
+                    <Link href='#/home'>
+                        <Image src={logo} alt="logo" className="w-[7rem]"/>
+                    </Link>
+                </div>
+                <div className="flex gap-10 items-center">
+                    <ol className="list-none flex gap-8">
+                        <Link href='#/plans'><li>Plans</li></Link>
+                        <Link href='#/about'><li>About</li></Link>
+                        <Link href='#/contact'><li>Contact</li></Link>
+                    </ol>
+
+                    <button className="px-3 py-2 bg-black text-white rounded-md">
+                        Get Free Trial
+                    </button>
+                </div>
+            </div>
+        </nav>
+    )
+}
