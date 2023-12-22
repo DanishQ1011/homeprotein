a5904c129c8d67044e1298897f35bcf0f605e789
Shah Danish <shah.official1011@gmail.com>
2023-12-22 15:13:33 +0530

    About section implemented

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,31 @@
-import Navbar from "@/components/Navbar"
-import Hero from "@/components/Hero"
-import FreeCard from "@/components/FreeCard"
-import Breakfast from "@/components/Breakfast"
-import Plans from "@/components/Plans"
-import Linner from "@/components/Linner"
-import Plans2 from "@/components/Plans2"
-import Fact from "@/components/Fact"
+import SiteNavbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import FreeCard from "@/components/FreeCard";
+import Breakfast from "@/components/Breakfast";
+import Plans from "@/components/Plans";
+import Linner from "@/components/Linner";
+import Plans2 from "@/components/Plans2";
+import Fact from "@/components/Fact";
+import About from "@/components/About";
 
 export default function Home() {
-  return (<>
+  return (
+    <>
       <main className="px-9 mx-44">
-        <Navbar />
-        <Hero/>
-        <FreeCard/>
-        <Breakfast/>
-        <Plans/>
-        <FreeCard/>
-        <Linner/>
-        <Plans2/>
-        <FreeCard/>
+        <SiteNavbar />
+        <Hero />
+        <FreeCard />
+        <Breakfast />
+        <Plans />
+        <FreeCard />
+        <Linner />
+        <Plans2 />
+        <FreeCard />
       </main>
-      <Fact/>
-      </>
-  )
+      <Fact />
+      <section className="px-9 mx-44">
+        <About />
+      </section>
+    </>
+  );
 }
