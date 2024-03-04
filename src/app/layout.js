bb995fe215367c2dc0b2c88baccc1a7a1c98ec25
Shah Danish <shah.official1011@gmail.com>
2024-03-04 22:00:19 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
-// import Hotjar from '@hotjar/browser';
+import Hotjar from '@hotjar/browser';
 import './globals.css';
+import React, { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,24 +12,23 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // Initialize Hotjar
-  // const siteId = 3890019;
-  // const hotjarVersion = 6;
-  // Hotjar.init(siteId, hotjarVersion);
+  const siteId = 3890019;
+  const hotjarVersion = 6;
+  Hotjar.init(siteId, hotjarVersion);
 
   return (
     <html lang="en">
       <head>
-      <Script>
-          (function(h,o,t,j,a,r){`
+      {/* <script>
+          (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
               h._hjSettings={hjid:3890019,hjsv:6};
               a=o.getElementsByTagName('head')[0];
               r=o.createElement('script');r.async=1;
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
               a.appendChild(r);
-          `})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-      </Script>
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      </script> */}
 
         <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-SXXTS7KV2L" />
         <Script strategy="afterInteractive" id="google-analytics">
