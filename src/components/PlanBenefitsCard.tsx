07818cd440d9144dd3d6c7c55342e8afa77feb71
Devakumar NM <pydev21@gmail.com>
2024-03-10 12:09:37 +0530

    refactor+feat(Plans2.jsx): add new plan toggle

---
+++ src/components/PlanBenefitsCard.tsx
@@ -0,0 +1,40 @@
+import checkmark from "@/assets/checkmark.svg";
+import Image from "next/image";
+
+const PlanBenefitsCard = ({ title, subTitle, benefits, subPrice }) => {
+  return (
+    <div className="p-[50px] w-[30rem] h-[31rem] max-sm:w-[20rem] max-sm:h-[36rem] rounded-md shadow-2xl max-lg:shadow-lg">
+      <div className="flex flex-col gap-3 items-center">
+        <h1 className="text-primary2 text-[24px] font-semibold">{title}</h1>
+        <p className="text-primary2">{subTitle}</p>
+        <h1 className="mt-4 text-[32px] font-semibold text-primary2">
+          {subPrice}
+          <span className="text-[16px] font-normal">/ month</span>{" "}
+        </h1>
+      </div>
+      <div className="mt-5">
+        <ul className="list-none text-sm text-primary2 leading-[2]">
+          {benefits.map((benefit) => (
+            <li className="flex items-center">
+              <Image
+                src={checkmark}
+                alt="check"
+                className="stroke-orange-300"
+              />
+              <span className="ml-2">{benefit}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex justify-center">
+        <a href="https://show.forms.app/devnm21/subscription-trial">
+          <button className="mt-7 w-[25rem] max-sm:w-[16rem] py-4 bg-black font-semibold text-white border-[1.5px] rounded-lg">
+            Subscribe Now
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default PlanBenefitsCard;
--- src/components/Plans2.jsx
+++ src/components/Plans2.jsx
@@ -1,67 +1,94 @@
-import Link from "next/link";
-import checkmark from '@/assets/checkmark.svg'
+"use client";
+import checkmark from "@/assets/checkmark.svg";
+import useToggle from "@/hooks/useToggle";
 import Image from "next/image";
+import Switch from "react-switch";
+import { formatIndianNumber } from "@/utils/formatting";
+import { priceConfig } from "@/utils/priceConfig";
+import PlanBenefitsCard from "@/components/PlanBenefitsCard";
 
-export default function Plan(){
-    return(
-        <section className="mt-10">
-            <div className="flex flex-col gap-4 items-center">
-                <h1 className="text-[34px] text-primary2 font-semibold text-center max-sm:w-[22rem]">Affordable Dinner and Lunch Choices.</h1>
-                <p className="text-[18px] text-center max-sm:w-[25rem]">Savor a satisfying midday break without breaking the bank, thanks to our wallet-friendly Lunch Meal options.</p>
-            </div>
+export default function Plan() {
+  const [isChecked, toggleSwitch] = useToggle(true);
+  const selectedPlan = isChecked ? "high" : "balanced";
+  const displayConfig = priceConfig[selectedPlan];
 
-            <div className="mt-10 flex gap-5 max-lg:flex-col max-lg:items-center justify-center">
-                <div className="p-[50px] w-[30rem] h-[31rem] max-sm:w-[20rem] max-sm:h-[36rem] rounded-md shadow-2xl max-lg:shadow-lg">
-                    <div className="flex flex-col gap-3 items-center">
-                        <h1 className="text-primary2 text-[24px] font-semibold">Weekly Plan</h1>
-                        <p className="text-primary2">Monthly flavors, crafted with care.</p>
-                        <h1 className="mt-4 text-[32px] font-semibold text-primary2">₹1,694<span className="text-[16px] font-normal">/ week</span> </h1>
-                    </div>
-                    <div className="mt-5">
-                    <ul className="list-none text-sm text-primary2 leading-[2]">
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">7 meals</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">₹242 per meal </span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">Both Lunch and Dinner at ₹3,300</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">40gm+ protein per meal</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">Super protein at extra ₹90 per meal (+30g protien)</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">Delivery charges included</span></li>
-                    </ul>
-                    </div>
-                    <div className="flex justify-center">
-                        <a href='https://show.forms.app/devnm21/subscription-trial'>
-                            <button className="mt-7 w-[25rem] max-sm:w-[16rem] py-4 border-black font-semibold shadow-xl hover:shadow-none text-black border-[1.5px] rounded-md">
-                                Subscribe Now
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                <div>
-                <div className="p-[50px] w-[30rem] h-[31rem] max-sm:w-[20rem] max-sm:h-[36rem] rounded-md shadow-2xl max-lg:shadow-lg">
-                    <div className="flex flex-col gap-3 items-center">
-                        <h1 className="text-primary2 text-[24px] font-semibold">Monthly Plan</h1>
-                        <p className="text-primary2">Fueling Your Week with Protein.</p>
-                        <h1 className="mt-4 text-[32px] font-semibold text-primary2">₹5,899<span className="text-[16px] font-normal">/ month</span> </h1>
-                    </div>
-                    <div className="mt-5">
-                    <ul className="list-none text-sm text-primary2 leading-[2]">
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">30 meals</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">₹196 per meal </span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">Both Lunch and Dinner at ₹9,899</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">40gm+ protein per meal</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">Super protein at extra ₹60 per meal (+30g protien)</span></li>
-                            <li className="flex items-center"><Image src={checkmark} alt="check" className="stroke-orange-300"/><span className="ml-2">Delivery charges included</span></li>
-                    </ul>
-                    </div>
-                    <div className="flex justify-center">
-                        <a href='https://show.forms.app/devnm21/subscription-trial'>
-                            <button className="mt-7 w-[25rem] max-sm:w-[16rem] py-4 bg-black font-semibold text-white border-[1.5px] rounded-lg">
-                                Subscribe Now
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </section>
-    )
+  const monthlyPricePerMeal = formatIndianNumber(
+    displayConfig?.monthly?.perMealPrice
+  );
+  const weeklyPricePerMeal = formatIndianNumber(
+    displayConfig?.weekly?.perMealPrice
+  );
+
+  const monthlyDiscountedLinnerPrice = formatIndianNumber(
+    displayConfig?.monthly?.linnerDiscountedPrice
+  );
+  const weeklyDiscountedLinnerPrice = formatIndianNumber(
+    displayConfig?.weekly?.linnerDiscountedPrice
+  );
+
+  const proteinContent = selectedPlan === "high" ? "35gm+" : "15-20gm";
+
+  const monthylSubPrice = formatIndianNumber(
+    displayConfig?.monthly?.totalSubPrice
+  );
+  const weeklySubPrice = formatIndianNumber(
+    displayConfig?.weekly?.totalSubPrice
+  );
+
+  const weeklyPlanBenefits = [
+    "7 Meals",
+    `${weeklyPricePerMeal} per meal`,
+    `Both Lunch and Dinner ${weeklyDiscountedLinnerPrice}`,
+    `${proteinContent} proten per meal`,
+    "Delivery charges included",
+  ];
+
+  const monthlyPlanBenefits = [
+    "30 Meals",
+    `${monthlyPricePerMeal} per meal`,
+    `Both Lunch and Dinner ${monthlyDiscountedLinnerPrice}`,
+    `${proteinContent} proten per meal`,
+    "Delivery charges included",
+  ];
+
+  return (
+    <section className="mt-10">
+      <div className="flex flex-col gap-4 items-center">
+        <h1 className="text-[34px] text-primary2 font-semibold text-center max-sm:w-[22rem]">
+          Affordable Dinner and Lunch Choices.
+        </h1>
+        <p className="text-[18px] text-center max-sm:w-[25rem]">
+          Savor a satisfying midday break without breaking the bank, thanks to
+          our wallet-friendly Lunch Meal options.
+        </p>
+      </div>
+
+      {/* SWITCH */}
+
+      <div className="w-full flex flex-col items-center py-10">
+        <div className="flex flex-row gap-5 items-center">
+          <span>Balanced Meal (15-20gm protein)</span>
+          <Switch onChange={toggleSwitch} checked={isChecked} />
+          <span>High Protein Meal (35gm protein)</span>
+        </div>
+      </div>
+
+      {/* PLANS */}
+
+      <div className="mt-10 flex gap-5 max-lg:flex-col max-lg:items-center justify-center">
+        <PlanBenefitsCard
+          benefits={weeklyPlanBenefits}
+          subPrice={weeklySubPrice}
+          subTitle={"Fueling Your Week with Protein."}
+          title={"Weekly Plan"}
+        />
+        <PlanBenefitsCard
+          benefits={monthlyPlanBenefits}
+          subPrice={monthylSubPrice}
+          subTitle={"Monthly flavors, crafted with care."}
+          title={"Monthly Plan"}
+        />
+      </div>
+    </section>
+  );
 }
