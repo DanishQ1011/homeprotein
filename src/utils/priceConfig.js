7500d8d4e5293f675c26d42fc8b559604ee5f995
Devakumar NM <pydev21@gmail.com>
2024-03-10 12:09:05 +0530

    feat: add `useToggle` hook, price config and formatting utils

---
+++ src/utils/priceConfig.js
@@ -0,0 +1,26 @@
+export const priceConfig = {
+    high: {
+        monthly: {
+            totalSubPrice: 5899,
+            perMealPrice: 196,
+            linnerDiscountedPrice: 2400
+        },
+        weekly: {
+            totalSubPrice: 1694,
+            perMealPrice: 242,
+            linnerDiscountedPrice: 2400    
+        },
+    },
+    balanced: {
+        monthly: {
+            totalSubPrice: 4650,
+            perMealPrice: 155,
+            linnerDiscountedPrice: 2400    
+        },
+        weekly: {
+            totalSubPrice: 1330,
+            perMealPrice: 190,
+            linnerDiscountedPrice: 2400
+        },
+    }
+}
