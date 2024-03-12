be561b5e9cf08e7afe6ac54bd7b05f99b888800c
Shah Danish <shah.official1011@gmail.com>
2024-03-12 16:19:55 +0530

    Update FAQ.jsx

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -4,16 +4,16 @@ import Accordion from './Accordion'
 const FAQ = () => {
   return (
     <section id='section' className='border-b-2'>
-        <div className="flex flex-col items-center justify-center p-12">
+        <div className="flex flex-col items-center justify-center px-10 py-2 mb-[4rem]">
             <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg">
-                <h2 className="text-2xl text-primary2 font-bold text-center mb-3 max-sm:w-[20rem] ">
+                <h2 className="text-2xl text-primary2 font-bold text-center mb-3 max-sm:w-[30rem] ">
                     Frequently Asked Questions
                 </h2>
-                <p className="text-md text-center mb-4">
+                <p className="text-md text-center mb-4 max-sm:w-[18rem]">
                     Got a question? We've got answers. If you have some other questions, contact us using email.
                 </p>
             </div>
-            <div className='px-4 py-2 w-1/2 bg-gray-100 rounded-lg  '>
+            <div className='px-4 py-2 w-1/2 max-sm:w-full bg-gray-100 rounded-lg  '>
             <Accordion
                 title={
                     <h1>Question 1</h1>
