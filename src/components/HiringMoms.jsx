001fb409e83c7f2ec12f51a1b3b8779b8ed8c59b
Shah Danish <shah.official1011@gmail.com>
2024-03-24 22:21:31 +0530

    Update HiringMoms.jsx

--- src/components/HiringMoms.jsx
+++ src/components/HiringMoms.jsx
@@ -13,17 +13,17 @@ const HiringMoms = () => {
     <>
       <Navbar />
       <main className="mx-44 mt-[110px] max-lg:mx-auto">
-        <h1 className="text-center text-[36px] text-primary1 font-semibold max-sm:text-[30px]">Mom Onboard Process</h1>
+        <h1 className="text-center text-[42px] text-primary1 font-semibold max-sm:text-[32px]">Mom Onboard Process</h1>
         <div className="flex flex-row-reverse gap-10 max-sm:flex-col-reverse mt-3">
           <div className="flex flex-col w-full">
             
-            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
+            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 hover:bg-primary1 transition ease-in-out duration-300 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
               <div className="relative">
               <h2 className="font-semibold text-[28px]">Step 1. Initial Application and Phone Interview</h2>
-              <ul className="mt-3 text-[18px] list-disc ml-4">
+              <ul className="mt-3 text-[18px] list-disc ml-4 ">
                 <li>Fill out our initial form providing basic information in the link given below</li>
-                <li>You will then receive a phone interview session, which can be either a video call or voice call.</li>
-                <li>Based on our requirements we will confirm availability and assess basic qualifications.</li>
+                <li>You will then receive a phone interview session, which can be either a video call or voice call, depending on your preference.</li>
+                <li>Once we've checked our needs and made sure you're a good fit, we'll let you know <br/> if you're in!</li>
               </ul>
               <a href="https://forms.gle/ELdCY76sxt916Q1K9" target="_blank" className="max-sm:flex justify-center mr-4">
                   <button className="mt-6 mb-2 px-3 py-2 w-1/4 max:sm-ml-4 bg-black text-white rounded-md max-lg:w-3/4">
@@ -34,10 +34,10 @@ const HiringMoms = () => {
               <Image src={step1} alt="Step1Image" width={300} height={250} className="rounded-xl transform scale-x-[-1]" />
             </div>
 
-            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
+            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 hover:bg-primary1 transition ease-in-out duration-300 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
               <div>
                 <h2 className="font-semibold text-[28px]">Step 2. Home Visit/Inspection</h2>
-                <p className="text-[18px] mt-3">We will schedule a home visit to conduct an inspection focused on following hygiene and beneficial practices</p>
+                <p className="text-[18px] mt-3">We'll plan a visit to your home to check things out and make sure you're following good hygiene practices.</p>
                 <ul className="mt-2 text-[18px] list-disc ml-4">
                   <li>Waste Disposal: Dispose of waste in covered bins with proper trash bags for sanitation.</li>
                   <li>Appliance Maintenance: Maintain kitchen appliances in good working order to ensure safe and efficient food preparation.</li>
@@ -47,20 +47,20 @@ const HiringMoms = () => {
               <Image src={step2} alt="Step2Image" width={300} height={250} className="rounded-xl transform scale-x-[-1]" />
             </div>
 
-            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
+            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 hover:bg-primary1 transition ease-in-out duration-300 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
               <div className="relative">
               <h2 className="font-semibold text-[28px]">Step 3. Taste Testing</h2>
               <p className="text-[18px] mt-3">Taste testing is conducted to assess cooking skills and preferences.</p>
               <ul className="mt-3 text-[18px] list-disc ml-4">
                 <li>Choose Fresh Ingredients: Highlight your knack for selecting the freshest ingredients, essential for delicious Indian cuisine.</li>
-                <li>Ensure Quality and Consistency: Present your dishes for final evaluation, ensuring they consistently meet our standards for taste and presentation..</li>
+                <li>Ensure Quality and Consistency: Present your dishes for final evaluation, ensuring they consistently meet the standards for taste and presentation.</li>
               </ul>
               <p className="absolute text-[15px] bottom-0 max-lg:static max-lg:mt-6">(Note: This step can consist initial & final taste testing depending on the cook and may take longer to process)</p>
               </div>
               <Image src={step3} alt="Step3Image" width={300} height={250} className="rounded-xl object-cover" />
             </div>
 
-            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
+            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 hover:bg-primary1 transition ease-in-out duration-300 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
               <div>
               <h2 className="font-semibold text-[28px]">Step 4. Recipes Training</h2>
               <p className="text-[18px] mt-3">Training is divided into two modules:</p>
@@ -73,7 +73,7 @@ const HiringMoms = () => {
               <Image src={step4} alt="Step4Image" width={300} className="rounded-xl object-cover transform scale-x-[-1]" />
             </div>
 
-            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
+            <div className="p-5 mt-6 max-sm:flex-col max-sm:items-center bg-gray-100 hover:bg-primary1 transition ease-in-out duration-300 rounded-lg flex gap-[2rem] border-primary1 border-[2px] text-[20px] font-semibold">
               <div>
               <h2 className="font-semibold text-[28px]">Step 5. Licensing and Onboarding</h2>
               <p className="text-[18px] mt-3">
