34a09c346875cf271d64da77f7815cfb6cea8426
DanishQ1011 <shah.official1011@gmail.com>
2024-04-24 20:36:17 +0530

    Cancellation and Refund Policy Added

---
+++ pages/CancellationAndRefundPolicy.jsx
@@ -0,0 +1,14 @@
+import '../src/app/globals.css';
+import Cancellation from "@/components/Cancellation";
+
+const CancellationAndRefundPolicy = () => {
+  return (
+    <div className="w-full h-screen ">
+      <div className="p-4">
+        <Cancellation/>
+      </div>
+    </div>
+  )
+}
+
+export default CancellationAndRefundPolicy
